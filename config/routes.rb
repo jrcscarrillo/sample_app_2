591bcdaf254c811f570ce5815a1e94b1f9892a9e
Juan Carrillo <jcarrillo1@madisoncollege.edu>
2013-03-06 10:38:35 -0600

    start sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp2::Application.routes.draw do
-
+    
+    resources :users
     root to: 'static_pages#home'
     match '/signup', to: 'users#new'
     match '/about', to: 'static_pages#about'
