20353dc6991a1e5a6685c75b1f97dbe85d55be29
Juan Carrillo <jcarrillo1@madisoncollege.edu>
2013-03-13 13:14:31 -0500

    Exercises with changes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,11 +2,26 @@ require 'spec_helper'
 
 describe "UserPages" do
     subject { page }
-    
-    describe "signup page" do
-
-        before { visit signup_path }
+ 
+     describe "profile page" do
         
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit user_path(user) }
+        
+        it { should have_selector('h1', text: user.name) }
+        
+        it { should have_selector('title', text: user.name) }
+        
+     end
+     
+    describe "signup page" do
+        before { visit signup_path }
+        it { should have_selector('h1', text: 'Sign Up') }
+        it { should have_selector('title', text: full_title('Sign Up')) }
+    end
+        
+    describe "signup" do
+        before { visit signup_path }
         let(:submit) { "Create my account" }
         
         describe "with invalid information" do
@@ -43,21 +58,5 @@ describe "UserPages" do
 
             end
         end
-        
-        it { should have_selector('h1', text: 'Sign Up') }
-      
-        it { should have_selector('title', text: full_title('Sign Up')) }
-      
   end
-
-    describe "profile page" do
-        
-        let(:user) { FactoryGirl.create(:user) }
-        before { visit user_path(user) }
-        
-        it { should have_selector('h1', text: user.name) }
-        
-        it { should have_selector('title', text: user.name) }
-        
-    end
 end
